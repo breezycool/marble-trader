a84ec71e5f45df204d3e670b748b42dee77e74ea
Dan K <danielkermode@hotmail.co.nz>
2015-12-28 15:47:18 +0000

    Revert "added loginregister"
    This reverts commit 99be0330c00da613c33541da4d2bea79a22ac08c.

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
-import LoginRegister from './LoginRegister.js';
 
 export default class Main extends React.Component {
 
   render() {
     return (
-    	<div>
-          <div>This is MarbleTrader</div>
-          <LoginRegister/>
-        </div>
+      <div>This is MarbleTrader</div>
     );
   }
 }
