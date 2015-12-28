99be0330c00da613c33541da4d2bea79a22ac08c
Dan K <danielkermode@hotmail.co.nz>
2015-12-28 15:43:29 +0000

    added loginregister

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import LoginRegister from './LoginRegister.js';
 
 export default class Main extends React.Component {
 
   render() {
     return (
-      <div>This is MarbleTrader</div>
+    	<div>
+          <div>This is MarbleTrader</div>
+          <LoginRegister/>
+        </div>
     );
   }
 }
