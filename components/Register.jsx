05ac9b4e40db388ad64655cb261e503393bf5d16
Dan K <danielkermode@hotmail.co.nz>
2015-12-28 18:39:30 +0000

    added register view

--- components/Register.jsx
+++ components/Register.jsx
@@ -8,31 +8,21 @@ var fieldValues = {
 
 export default class Register extends React.Component {
   render() {   
-      
-      let styles = {.container {
-                      width: 500px;
-                      clear: both;
-                      }
-
-             .container-input {
-                width: 100%;
-                clear: both;
-              }
     
     return (
 
-      <div className={styleMap.container}>
+      <div>
         <div>
           <label>Name: </label>
-          <input type="text" ref="name" className={styleMap.containerInput} defaultValue={fieldValues.name} />
+          <input type="text" ref="name" defaultValue={fieldValues.name} />
         </div>
         <div>
           <label>Password: </label>
-          <input type="password" ref="password" className={styleMap.containerInput} defaultValue={fieldValues.password} />
+          <input type="password" ref="password" defaultValue={fieldValues.password} />
         </div>
         <div>
           <label>Email: </label>
-          <input type="email" ref="email" className={styleMap.containerInput} defaultValue={fieldValues.email} />
+          <input type="email" ref="email" defaultValue={fieldValues.email} />
         </div>
         <div>
           <button className="btn btn-warning btn-lg glyphicon glyphicon-user" onClick={this.onClick}> Register!</button>
