1fd704e348af3ef27842abb5f5683c59b80ed1ac
Dan K <danielkermode@hotmail.co.nz>
2015-12-28 18:36:25 +0000

    made registerview

--- components/Login.jsx
+++ components/Login.jsx
@@ -5,8 +5,8 @@ export default class Login extends React.Component {
   render() {
     return (
       <div>
-         <Link className="btn btn-success btn-lg glyphicon glyphicon-log-in" to='login'> Login!</Link> <span> | </span>
-         <Link className="btn btn-success btn-lg glyphicon glyphicon-user" to='register'> Register!</Link> <span> | </span> 
+         <Link className="btn btn-success btn-lg glyphicon glyphicon-log-in" to='login'> Login!</Link> <span> </span>
+         <Link className="btn btn-success btn-lg glyphicon glyphicon-user" to='register'> Register!</Link> <span> </span> 
          <Link className="btn btn-info btn-lg glyphicon glyphicon-info-sign" to='about'> About</Link>
       </div>
     )
--- components/Register.jsx
+++ components/Register.jsx
@@ -7,19 +7,36 @@ var fieldValues = {
 }
 
 export default class Register extends React.Component {
-  render() {
+  render() {   
+      
+      let styles = {.container {
+                      width: 500px;
+                      clear: both;
+                      }
+
+             .container-input {
+                width: 100%;
+                clear: both;
+              }
+    
     return (
-      <div>
-        <label>Name</label>
-        <input type="text" ref="name" defaultValue={fieldValues.name} />
- 
-        <label>Password</label>
-        <input type="password" ref="password" defaultValue={fieldValues.password} />
- 
-        <label>Email</label>
-        <input type="email" ref="email" defaultValue={fieldValues.email} />
- 
-        <button onClick={this.onClick}>Register</button>
+
+      <div className={styleMap.container}>
+        <div>
+          <label>Name: </label>
+          <input type="text" ref="name" className={styleMap.containerInput} defaultValue={fieldValues.name} />
+        </div>
+        <div>
+          <label>Password: </label>
+          <input type="password" ref="password" className={styleMap.containerInput} defaultValue={fieldValues.password} />
+        </div>
+        <div>
+          <label>Email: </label>
+          <input type="email" ref="email" className={styleMap.containerInput} defaultValue={fieldValues.email} />
+        </div>
+        <div>
+          <button className="btn btn-warning btn-lg glyphicon glyphicon-user" onClick={this.onClick}> Register!</button>
+        </div>
       </div>
     )
   }
