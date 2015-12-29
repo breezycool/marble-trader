31bb033993f21422e94b75b24e77f99c42057450
breezycool <lachlankermode@live.com>
2015-12-29 00:20:35 +0000

    basic testing suite for user reducer

--- components/App.jsx
+++ components/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router'
 import Scene from './3D-Scene/Scene.js'
-import NavBar from './NavBar'
+import NavBarView from './NavBarView'
 
 export default class App extends React.Component {
 	render() {
